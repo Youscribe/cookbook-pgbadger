d1f172545895a517019da000fd6b9f1f22fe0f08
Guilhem Lettron <guilhem@lettron.fr>
2012-09-19 12:13:51 +0200

    release v0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ name             "pgbadger"
 license          "Apache 2.0"
 description      "Install pgbadger"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "apt"
 supports         "ubuntu"
 recipe           "install", "Install pgbadger on the node"
